80198d96fe9aabe1dc74df4348fe18e5b8c7d7fa
mds <17163988+mds1@users.noreply.github.com>
2020-02-28 12:00:46 -0500

    Cleanup old contract & test code

--- contracts/Faker.sol
+++ contracts/Faker.sol
@@ -59,24 +59,10 @@ contract Faker {
 
   // Variables for managing phases
   uint256 public deploymentTime; // needed to determine the current period
-  uint256 public periodLength; // default 1 day
-  uint256 constant shiftPhaseLength = 1;    // default 1 periods
-  uint256 constant auctionPhaseLength = 1;  // default 1 periods
-  uint256 constant phaseLength = 7;   // default 7 periods
-  uint256 public votingPhaseLength; // TODO delete this
-
-  // Variables for managing deposits
-  uint256 public totalDeposited;
-  address[] public depositors;
-  mapping (address => uint256) public balances;
-
-  // Variables for managing bidders
-  struct Bid {
-    address bidder;
-    uint256 amount;
-  }
-  mapping (uint256 => Bid) public bids; // mapping of phase number to current leading bid
-  mapping (address => uint256) public refundAmount; // mapping of addresses to their available refund
+  uint256 public periodLength; // 1 day
+  uint256 constant phaseLength = 7;   // 7 periods
+  uint256 constant shiftPhaseLength = 1;    // 1 periods
+  uint256 constant auctionPhaseLength = 1;  // 1 periods
 
   constructor(uint256 _periodLength) public {
     // TODO do the below setup here?
@@ -86,60 +72,6 @@ contract Faker {
     deploymentTime = now;
     periodLength = _periodLength;
   }
-
-  function submitBid(uint256 _useRefundAmount) external payable {
-    // Get phase and current bid info
-    (bool _isAuctionActive, uint256 _auctionPhase) = getCurrentAuctionPhase();
-    require(_isAuctionActive, "Faker: No auction is ongoing");
-    Bid memory _leadingBid = bids[_auctionPhase];
-
-    // Make sure bidder refund inclusion amount is valid
-    require(_useRefundAmount <= refundAmount[msg.sender], "Faker: Refund input too high");
-
-    // Adjust refund available to the losing bidder (they are still leading bidder)
-    refundAmount[_leadingBid.bidder] = refundAmount[_leadingBid.bidder].add(_leadingBid.amount);
-
-    // Adjust refund available to the new winning bidder
-    refundAmount[msg.sender] = refundAmount[msg.sender].sub(_useRefundAmount);
-
-    // Calculate total bid amount of sender and update bid
-    uint256 _bidAmount = msg.value.add(_useRefundAmount);
-    require(_bidAmount > _leadingBid.amount, "Faker: Bid is less than current leader");
-    bids[_auctionPhase] = Bid(msg.sender, _bidAmount); // update leading bid for current phase
-  }
-
-  function claimRefund() external {
-    // Prevent calling if no refund is available
-    uint256 _refundAmount = refundAmount[msg.sender];
-    require(_refundAmount > 0, "Faker: No refund available for caller");
-
-    // Update state
-    refundAmount[msg.sender] = 0;
-
-    msg.sender.transfer(_refundAmount);
-  }
-
-  function submitVote() external {
-  }
-
-  function deposit(uint256 _mkrAmount) external {
-    // TODO
-    require(_mkrAmount > 0, "Faker: Deposit amount must be greater than zero");
-    // User must approve this contract to spend their MKR
-    // Transfer MKR from the user to this contract
-    // mkrContract.transferFrom(msg.sender, address(this), _mkrAmount);
-    // MKR address: 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
-
-    // Update their MKR balance
-    totalDeposited.add(_mkrAmount);
-    balances[msg.sender].add(_mkrAmount);
-
-    // Add user to list of all depositors
-    if (balances[msg.sender] == 0) {
-      depositors.push(msg.sender);
-    }
-  }
-
 
   // ======================================== Helpers ========================================
   function getCurrentPeriod() public view returns (uint256) {
@@ -158,34 +90,4 @@ contract Faker {
   function isAuction() public view returns (bool) {
     return (getCurrentPeriod() % phaseLength == 1);
   }
-
-  // BELOW THIS IS OUTDATED!
-  function getCurrentVotingPhase() public view returns (bool isVoteActive, uint256 votePhase) {
-    uint256 _period = getCurrentPeriod();
-
-    // If we are in the first two days after deployment, voting has not yet started
-    if (_period < auctionPhaseLength) {
-      return (false, 0);
-    }
-
-    // Otherwise, return the current voting phase number
-    uint256 _phase = _period.sub(auctionPhaseLength).div(votingPhaseLength);
-    return (true, _phase);
-  }
-
-  function getCurrentAuctionPhase() public view returns (bool isAuctionActive, uint256 auctionPhase) {
-     // If we are in the first two days after deployment, this is the first auction period
-    (bool _isVotingActive, uint256 _votingPhase) = getCurrentVotingPhase();
-    if (!_isVotingActive) {
-      return (true, 0);
-    }
-
-    uint256 _period = getCurrentPeriod();
-    // If modulus day, we are in an auction phase
-    uint256 _modulusResult = _period % votingPhaseLength;
-    bool _isModulusDay = _modulusResult < auctionPhaseLength;
-
-    return (_isModulusDay, _votingPhase + 1);
-  }
-
 }
---
+++ test/faker-time.js
@@ -0,0 +1,124 @@
+const { time, expectRevert } = require("@openzeppelin/test-helpers");
+// time docs: https://docs.openzeppelin.com/test-helpers/0.5/api#time
+
+const Faker = artifacts.require("Faker");
+
+contract("Faker", accounts => {
+  let instance = null;
+  let periodLength = null;
+  const utils = web3.utils;
+  const BN = utils.BN;
+
+  const bidder1 = accounts[1];
+  const firstBidAmount = utils.toWei("1", "ether");
+  const invalidRefundUseAmount = utils.toWei("1.1", "ether");
+  const bidAdditionAmount = utils.toWei("0.51", "ether");
+  const winningBidAmount = utils.toWei("1.51", "ether");
+
+  const bidder2 = accounts[2];
+  const secondBidAmount = utils.toWei("0.5", "ether");
+  const thirdBidAmount = utils.toWei("1.5", "ether");
+
+  before(async () => {
+    instance = await Faker.deployed();
+    periodLength = await instance.periodLength();
+  });
+
+  it("should should find the contract instance", async () => {
+    assert(instance.address.startsWith("0x"), "Deployment not found");
+  });
+
+  // PERIOD 0
+
+  it("should start at period 0", async () => {
+    const period = await instance.getCurrentPeriod();
+    assert.equal(period, 0, "Invalid initial period");
+  });
+
+  it("should be in phase 0 during the 0th period", async () => {
+    const phase = await instance.getCurrentPhase();
+    assert.equal(phase, 0, "Incorrect phase for period");
+  });
+
+  it("shift should be active during period 0", async () => {
+    const isShift = await instance.isShift();
+    assert(isShift, "Invalid shift status");
+  });
+
+  it("auction should not be active during period 0", async () => {
+    const isAuction = await instance.isAuction();
+    assert(!isAuction, "Invalid auction status");
+  });
+
+  // PERIOD 1
+
+  it("auction should be active during period 1", async () => {
+    await time.increase(periodLength);
+    const isAuction = await instance.isAuction();
+    assert(isAuction, "Invalid auctions status");
+  });
+
+  it("shift should not be active during period 1", async () => {
+    const isShift = await instance.isShift();
+    assert(!isShift, "Invalid shift status");
+  });
+
+  // PERIOD 2
+
+  it("auction should not be active during period 2", async () => {
+    await time.increase(periodLength);
+    const isAuction = await instance.isAuction();
+    assert(!isAuction, "Invalid auctions status");
+  });
+
+  it("shift should not be active during period 2", async () => {
+    const isShift = await instance.isShift();
+    assert(!isShift, "Invalid shift status");
+  });
+
+  // PERIOD 7
+
+  it("should be in period 7", async () => {
+    await time.increase(5 * periodLength);
+    const period = await instance.getCurrentPeriod();
+    assert.equal(period, 7, "Invalid period");
+  });
+
+  it("should be in phase 1 during the 7th period", async () => {
+    const phase = await instance.getCurrentPhase();
+    assert.equal(phase, 1, "Incorrect phase for period");
+  });
+
+  it("shift should be active during period 7", async () => {
+    const isShift = await instance.isShift();
+    assert(isShift, "Invalid shift status");
+  });
+
+  it("auction should not be active during period 7", async () => {
+    const isAuction = await instance.isAuction();
+    assert(!isAuction, "Invalid auction status");
+  });
+
+  // PERIOD 8
+
+  it("should be in period 8", async () => {
+    await time.increase(periodLength);
+    const period = await instance.getCurrentPeriod();
+    assert.equal(period, 8, "Invalid period");
+  });
+
+  it("should be in phase 1 during the 8th period", async () => {
+    const phase = await instance.getCurrentPhase();
+    assert.equal(phase, 1, "Incorrect phase for period");
+  });
+
+  it("shift should not be active during period 8", async () => {
+    const isShift = await instance.isShift();
+    assert(!isShift, "Invalid shift status");
+  });
+
+  it("auction should be active during period 8", async () => {
+    const isAuction = await instance.isAuction();
+    assert(isAuction, "Invalid auction status");
+  });
+});

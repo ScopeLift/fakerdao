50b4acf0daf676aacaaa7c0826a04a1f53eb6e2f
mds <17163988+mds1@users.noreply.github.com>
2020-03-10 09:22:05 -0700

    Add ffooter

--- app/src/layouts/BaseLayout.vue
+++ app/src/layouts/BaseLayout.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hhh Lpr fff">
+    <!-- HEADER -->
     <q-header
       class="q-mx-md q-mt-md"
       style="color: #000000; background-color: rgba(0,0,0,0)"
@@ -17,9 +18,51 @@
         </div>
       </div>
     </q-header>
+    <!-- MAIN CONTENT -->
     <q-page-container>
       <router-view />
     </q-page-container>
+    <!-- FOOTER -->
+    <q-footer
+      bordered
+      class="q-mt-xl"
+      style="color: #000000; background-color: rgba(0,0,0,0)"
+    >
+      <div class="row justify-center items-center q-my-xl">
+        <div class="text-caption">
+          This website, the corresponding
+          <a
+            href="https://www.scopelift.co/blog/fakerdao"
+            target="_blank"
+          >article</a>,
+          and the FakerDAO contract
+          <a
+            href="https://github.com/scopelift/fakerdao"
+            target="_blank"
+          >source code</a>
+          were co-authored by
+          <a
+            href="https://twitter.com/msolomon44"
+            target="_blank"
+          >Matt Solomon</a>
+          of
+          <a
+            href="https://floatify.net"
+            target="_blank"
+          >Floatify</a>
+          and
+          <a
+            href="https://twitter.com/BenDiFrancesco"
+            target="_blank"
+          >Ben DiFrancesco</a>
+          of
+          <a
+            href="https://www.scopelift.co/home"
+            target="_blank"
+          >ScopeLift</a>.
+        </div>
+      </div>
+    </q-footer>
   </q-layout>
 </template>
 
